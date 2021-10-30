50582e1771883f972720bcd04ea3c79bf90ed03a
brenobccc <brenobritobc@gmail.com>
2021-10-30 08:08:36 -0300

    Category class done

---
+++ backend/src/main/java/com/devsuperior/dscatalog/Category.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscatalog;
+import java.io.Serializable;//Converter o objeto em sequencia de bytes
+//ser gravados em arquivos, passado na rede.
+
+public class Category implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public Category(){
+
+    }
+
+    //getters and setters
+    public Category(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //método mais lento, mas com a comparação 100%
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Category category = (Category) object;
+        return java.util.Objects.equals(id, category.id) && java.util.Objects.equals(name, category.name);
+    }
+
+    //Objeto padrão para verificar se um objeto é igual a outro, mais rápido
+    /*public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name);
+    }*/
+}
--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DscatalogApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DscatalogApplication.class, args);
+		System.out.println("Testadno");
 	}
 
 }
